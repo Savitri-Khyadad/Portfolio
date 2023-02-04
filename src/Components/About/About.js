fc7392da98c45bbea5c3f1b2b21ac7e5d69f54c0
SavitriKhyadad <savitrikhyadad15@gmail.com>
2023-02-04 18:56:05 +0530

    Updated node and edited role in Home Page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,62 +1,73 @@
-import React from 'react';
-import './About.css';
+import React from "react";
+import "./About.css";
 
 const About = () => {
-    return (
-        <div className="about">
-            <p>A self-motivated and an ambitious individual seeking a career opportunity as a full stack web developer. A quick learner with exceptional problem solving and analytical skills with an ability to collaborate.</p>
-            <br></br>
-            <h3>Skills</h3>
-            <br></br>
-            <div className="row">
-                <div className="column">
-                <ul id="list">
-                    <li>C</li>
-                    <li>C++</li>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JavaScript</li>
-                    <li>Reactjs</li>
-                    <li>Python</li>
-                </ul>
-                </div>
-                <div className="column">
-                <ul id="list">
-                    <li>Machine Learning</li>
-                    <li>Tensorflow</li>
-                    <li>Keras</li>
-                    <li>MongoDB</li>
-                    <li>Nodejs</li>
-                    <li>Angular</li>
-                </ul>
-                </div>
-                <div className="column">
-                <ul id="list">
-                    <li>Bootstrap</li>
-                    <li>Java</li>
-                    <li>Git</li>
-                    <li>Latex</li>
-                </ul>
-                </div>
-            </div>
-            <br></br>
-            <h3>Internships</h3>
-            <br></br>
-            <p>Junior Web Developer Intern at <strong>Transil Technologies Private Limited</strong>.</p>
-            <br></br>
-            <p>Worked on a web based education application using React and acquired decision-making, 
-            communication and planning skills by working in a collaborative environment.</p>
-            <br></br>
-            <h3>Interests</h3>
-            <br></br>
-            <ul id="list">
-                    <li>Web Development</li>
-                    <li>Competitive Coding</li>
-                    <li>Badminton</li>
-                    <li>Sketching</li>
-            </ul>
-        </div >
-    )
-}
+  return (
+    <div className="about">
+      <p>
+        A self-motivated and an ambitious individual seeking a career
+        opportunity as a full stack web developer. A quick learner with
+        exceptional problem solving and analytical skills with an ability to
+        collaborate.
+      </p>
+      <br></br>
+      <h3>Skills</h3>
+      <br></br>
+      <div className="row">
+        <div className="column">
+          <ul id="list">
+            <li>C</li>
+            <li>C++</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>Reactjs</li>
+            <li>Python</li>
+          </ul>
+        </div>
+        <div className="column">
+          <ul id="list">
+            <li>Machine Learning</li>
+            <li>Tensorflow</li>
+            <li>Keras</li>
+            <li>MongoDB</li>
+            <li>Nodejs</li>
+            <li>Angular</li>
+          </ul>
+        </div>
+        <div className="column">
+          <ul id="list">
+            <li>Bootstrap</li>
+            <li>Java</li>
+            <li>Git</li>
+            <li>Latex</li>
+          </ul>
+        </div>
+      </div>
+      <br></br>
+      <h3>Internships</h3>
+      <br></br>
+      <p>
+        Junior Web Developer Intern at{" "}
+        <strong>Transil Technologies Private Limited</strong>.
+      </p>
+      <br></br>
+      <p>
+        Worked on a web based education application using React and acquired
+        decision-making, communication and planning skills by working in a
+        collaborative environment.
+      </p>
+      <br></br>
+      <h3>Interests</h3>
+      <br></br>
+      <ul id="list">
+        <li>Web Development</li>
+        <li>Competitive Coding</li>
+        <li>Badminton</li>
+        <li>Sketching</li>
+      </ul>
+    </div>
+  );
+};
 
 export default About;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <div className="Home">
-            <div className="Image">
-                <img src={`${process.env.PUBLIC_URL}/girl_laptop.png`} alt="This is me" />
-            </div>
-            <div className="desc">
-                <p>Hello</p>
-                <p>I am Savitri Khyadad</p>
-                <p>4th Year Computer Science Undergraduate</p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="Home">
+      <div className="Image">
+        <img
+          src={`${process.env.PUBLIC_URL}/girl_laptop.png`}
+          alt="This is me"
+        />
+      </div>
+      <div className="desc">
+        <p>Hello</p>
+        <p>I am Savitri Khyadad</p>
+        <p>Cloud Support Associate at Amazon Web Services(AWS)</p>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
