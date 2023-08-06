45d7c6ef368159980da5822a77ab7638c1e95c2e
SavitriKhyadad <savitrikhyadad15@gmail.com>
2023-08-06 15:55:04 +0530

    Added Redirect to home

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from './Components/Home/Home';
-import Footer from './Components/Footer/Footer';
-import About from './Components/About/About';
-import Navigationbar from './Components/Navigation/Navigationbar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Footer from "./Components/Footer/Footer";
+import About from "./Components/About/About";
+import Navigationbar from "./Components/Navigation/Navigationbar";
 import Carousels from "./Components/Projects/Projects";
 import Contact from "./Components/Contact/Contact";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <Navigationbar></Navigationbar>
-      <Switch>
-        <Route exact path="/" component={Home}></Route>
-        <Route exact path="/projects" component={Carousels}></Route>
-        <Route exact path="/about" component={About}></Route>
-        <Route exact path="/contact" component={Contact}></Route>
-      </Switch>
-      <Footer></Footer>
+        <Navigationbar></Navigationbar>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/projects" component={Carousels}></Route>
+          <Route exact path="/about" component={About}></Route>
+          <Route exact path="/contact" component={Contact}></Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+        <Footer></Footer>
       </div>
     </Router>
-    
   );
 }
 
